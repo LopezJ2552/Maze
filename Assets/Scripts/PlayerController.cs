1d34b2bc26323182f58e4d2e082da6d7e6401cd1
LopezJ2552 <javierlopez@knights.ucf.edu>
2019-07-14 14:37:14 -0400

    Second Commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,7 +17,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
     }
 
     void FixedUpdate()
